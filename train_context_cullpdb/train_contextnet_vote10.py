de27b7cf0fd42289a076bd9fbcbcd0b240b086ed
qzlshy <qzlshy@126.com>
2019-01-30 19:54:35 +0800

    Update train_contextnet_vote10.py

--- train_context_cullpdb/train_contextnet_vote10.py
+++ train_context_cullpdb/train_contextnet_vote10.py
@@ -3,9 +3,6 @@ import tensorflow as tf
 import math
 import random
 import context
-import os
-import scipy.stats as stats
-from scipy.stats import truncnorm
 
 slim = tf.contrib.slim
 
@@ -13,17 +10,20 @@ seqdic={'A':0, 'R':1, 'D':2, 'C':3, 'Q':4, 'E':5, 'H':6, 'I':7, 'G':8, 'N':9, 'L
 
 labeldic={'H':1,'G':1,'I':1, 'E':2,'B':2}
 
-data=np.load('cullpdb_train.npy').item()
+#load training dataset
+data=np.load('cullpdb_train.npy').item() 
 seq=data['seq']
 pssm_list=data['pssm']
 dssp=data['dssp']
 
+#get length of each sequence
 seq_l=[]
 for i in range(len(seq)):
 	seq_l.append(len(seq[i]))
 
 seq_l=np.array(seq_l)
 
+#get mean and standard deviation of training dataset
 pssm_rsp=[]
 for i in range(len(pssm_list)):
 	for j in range(len(pssm_list[i])):
@@ -32,26 +32,30 @@ for i in range(len(pssm_list)):
 cnt=np.mean(pssm_rsp)
 std=np.std(pssm_rsp)
 
+#normalization of training dataset
 for i in range(len(pssm_list)):
 	pssm_list[i]=pssm_list[i]-cnt
 	pssm_list[i]=pssm_list[i]/std
 
+#load test dataset
 test=np.load('cullpdb_test.npy').item()
 seq_test=test['seq']
 pssm_test_list=test['pssm']
 dssp_test=test['dssp']
 
-
+#get length of each sequence
 seq_l_test=[]
 for i in range(len(seq_test)):
 	seq_l_test.append(len(seq_test[i]))
 
 seq_l_test=np.array(seq_l_test)
 
+#normalization of est dataset
 for i in range(len(pssm_test_list)):
 	pssm_test_list[i]=pssm_test_list[i]-cnt
 	pssm_test_list[i]=pssm_test_list[i]/std
 
+#decode sequence and second structure to number
 seq_num=[]
 for a in seq:
 	tmp=np.zeros(len(a))
@@ -92,6 +96,7 @@ for a in dssp_test:
 			tmp[i]=0
 	dssp_num_test.append(tmp)
 
+#generate batch of training dataset 
 def g_data(n,m):
 	l=m
 	data1=np.zeros([l,20])
@@ -108,7 +113,7 @@ def g_data(n,m):
 	return data,label,z
 
 
-
+#generate batch of test dataset
 def g_data_test(n,m):
 	l=m
 	data1=np.zeros([l,20])
@@ -126,7 +131,7 @@ def g_data_test(n,m):
 
 training_epochs = 40
 
-
+#build model
 x=tf.placeholder("float",[None,None,1,20],name='input')
 y=tf.placeholder("float",[None,None,3],name='label')
 z=tf.placeholder("float",[None,None],name='z')
@@ -157,9 +162,8 @@ init = tf.global_variables_initializer()
 num=list(range(len(seq_num)))
 saver = tf.train.Saver(max_to_keep=0)
 
+#train model
 with tf.Session() as sess:
-	all_data=[]
-	all_pre=[]
 	for cw in range(10):
 		save_name="model/my-model-"+str(cw)
 		acc=0.0
@@ -180,42 +184,13 @@ with tf.Session() as sess:
 			print("train:",t1/t2)
 			t1=0.0
 			t2=0.0
-			eve_ac=[]
-			all_pret=[]
 			for i in range(len(seq_num_test)):
-				batch_x,batch_y,batch_z=g_data_test(i,len(seq_num_test[i]))
-				n1,n2,b_pre=sess.run([ac_num,tt2,pre],feed_dict={x:batch_x, y:batch_y, z:batch_z})
+				batch_x, batch_y, batch_z=g_data_test(i,len(seq_num_test[i]))
+				n1,n2=sess.run([ac_num,tt2],feed_dict={x:batch_x, y:batch_y, z:batch_z})
 				t1=t1+n1
 				t2=t2+n2
-				eve_ac.append(n1/n2)
-				all_pret.append(b_pre)
 			if t1/t2>acc:
 				saver.save(sess, save_name)
 				acc=t1/t2
-				f_eve_ac=eve_ac
-				f_all_pre=all_pret
 			print("acc:",t1/t2)
-		all_data.append(f_eve_ac)
-		all_pre.append(f_all_pre)
 
-
-t1=0
-t2=0
-for i in range(len(seq_l_test)):
-	d=[]
-	for j in range(len(all_pre)):
-		d.append(all_pre[j][i])
-	p=[]
-	l=seq_l_test[i]
-	for j in range(l):
-		ss=[0,0,0]
-		for k in range(len(d)):
-			ss[int(d[k][j])]+=1
-		p.append(np.argmax(ss))
-	for j in range(len(p)):
-		if p[j]==dssp_num_test[i][j]:
-			t1+=1
-		t2+=1
-	
-
-print('result:',t1/t2)
